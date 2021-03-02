9e2c3031998916510153e5188c271eed7f8ee66d
HuangTing <Ht@18211276722>
2021-03-02 11:43:02 +0800

    最近学习更新

--- Redux/redux-action.js
+++ Redux/redux-action.js
@@ -130,6 +130,71 @@ const fetchPosts = postTitle => (dispatch, getState) => {
 
 
 
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import reducer from './reducer'
+
+const store = createStore(
+    reducer,
+    applyMiddleware(thunk)
+);
+
+
+import {createStore, applyMiddleware} from 'redux'
+import promiseMiddleware from 'redux-promise'
+import reducer from './reducers';
+
+const store = createStore(
+    reducer,
+    applyMiddleware(promiseMiddleware)
+)
+
+const fetchPosts =
+    (dispatch, postTitle) => new Promise(function (resolve, reject) {
+        dispatch(requestPosts(postTitle));
+        return fetch(`/some/API/${postTitle}.json`)
+            .then(response => ({
+                type:'FETCH_POSTS',
+                payload:response.json()
+            }))
+    })
+
+
+import { createAction } from 'redux-actions';
+
+class AsyncApp extends Component {
+    componentDidMount() {
+        const {dispatch, selectedPost} = this.props
+        // 发出同步 Action
+        dispatch(requestPosts(selectedPost));
+        // 发出异步 Action
+        dispatch(createAction(
+            'FETCH_POSTS',
+            fetch(`/some/API/${postTitle}.json`)
+                .then(response => response.json())
+        ));
+    }
+}
+
+/*复习*/
+const ADD = 'ADD';
+const addMessage = (message) => {
+    return {
+        type: ADD,
+        message: message
+    }
+}
+const reducer = (previousState = [], action) => {
+    switch(action.type){
+        case ADD:
+            return [...previousState,action.message];
+            break;
+        default:
+            return previousState
+    }
+}
+
+const store = Redux.createStore(reducer);
 
 
 
@@ -138,5 +203,61 @@ const fetchPosts = postTitle => (dispatch, getState) => {
 
 
 
+
+
+
+/*测试*/
+import {Privider} from 'react-redux'
+import {createStore} from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
+
+let store = createStore(todoApp);
+
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+)
+
+/*
+* 上面代码中，Provider在根组件外面包了一层，这样一来，
+* App的所有子组件就默认都可以拿到state了。
+* 它的原理是React组件的context属性，请看源码。
+* */
+class Provider extends Component{
+    getChildContext() {
+        return {
+            store: this.props.store
+        };
+    }
+    render() {
+        return this.props.children;
+    }
+}
+Provider.childContextTypes = {
+    store: React.PropTypes.object
+}
+/*上面代码中，store放在了上下文对象context上面。
+然后，子组件就可以从context拿到store，代码大致如下。*/
+class VisibleTodoList extends Component {
+    componentDidMount(){
+        const {store} = this.context;
+        this.unsubscribe = store.subscribe(() => {
+            this.forceUpdate();
+        })
+    };
+
+    render(){
+        const props = this.props;
+        const {store} = this.context;
+        const state = store.getState();
+    }
+}
+
+VisibleTodoList.contextTypes = {
+    store: React.PropTypes.object
+}
 
 
